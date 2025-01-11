a8769adacf94a7570a624bfda450b6ea7a0952a0
HongCheng Liang <hongcheng2001@163.com>
2025-01-11 13:04:14 +0800

    Update precompute_Chebyshev.m

--- precompute_Chebyshev.m
+++ precompute_Chebyshev.m
@@ -1,7 +1,7 @@
 % This programme precomputes the coefficients of Chebyshev polynomials to reduce computational costs.
 
-% In this programme, cos(omega) to cos(200 * omega) are expanded into 
-% polynomials relating to only cos(omega). You can adjust the expansion number.
+% In this programme, cos(omega) to cos(200 * omega) are expanded as polynomials relating with only cos(omega). 
+% Please feel free to adjust the expansion number below.
 expand_num = 200; 
 expanded_polynomial = cell(1, expand_num);  % Cell array to store expanded polynomials
 syms y;  % Define symbolic variable
@@ -11,8 +11,7 @@ for m = 1:expand_num
     expanded_polynomial{m} = chebyshevT(m, y);  % Store each Chebyshev polynomial
 end
 
-% Using the expanded polynomials to construct combined polynomials:
-% cos((k-1) * omega) - cos(k * omega).
+% Using the expanded polynomials to construct combined polynomials: cos((k-1) * omega) - cos(k * omega).
 % Extract the coefficients of all combined polynomials for later use in the MAPR algorithm.
 all_coefficients = cell(1, expand_num);  % Cell array to store the coefficients
 for l = 2:expand_num
